11d00fb304a7996997a7ec20e7002a6e891b9b8d
boluwatife-David <boldav20000309@gmail.com>
2022-10-30 15:45:08 +0100

    hmm

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from "react";
 import {BsSlack, BsGithub} from "react-icons/bs"
-import {CiDark} from "react-icons/ci";
+import {TbArrowBackUp} from "react-icons/tb";
+import {GoPrimitiveDot} from "react-icons/go";
 import {BiWorld} from "react-icons/bi"
 import numb from "./numb.jpeg"
 
 function App() {
-  const [theme , setTheme] = React.useState(
-    localStorage.getItem("theme") || "light"
-  )
-  function toggleTheme () {
-    if (theme === "light") {
-      setTheme("dark")
-    } else {
-      setTheme("light")
-    }
+  // const [theme , setTheme] = React.useState(
+  //   localStorage.getItem("theme") || "light"
+  // )
+  // function toggleTheme () {
+  //   if (theme === "light") {
+  //     setTheme("dark")
+  //   } else {
+  //     setTheme("light")
+  //   }
     
-  }
-  React.useEffect(() => {
-    localStorage.setItem("theme", theme)
-    document.body.className = theme;
-  }, [theme])
+  // }
+  // React.useEffect(() => {
+  //   localStorage.setItem("theme", theme)
+  //   document.body.className = theme;
+  // }, [theme])
   return (
-    <div className={`App ${theme}`}>
+    <div>
       <header className="header">
-        <p><CiDark className="theme-btn" onClick={toggleTheme}/></p>
+        <p><TbArrowBackUp className="theme-btn" /></p>
         <img src={numb} alt="profile-img" className="skeng"/>
         <p>NUMB.</p>
       </header>
@@ -39,7 +40,7 @@ function App() {
      <hr />
      <footer className="footer">
       <div>
-        <h4>Zuri Internship</h4>
+        <h4>Zuri <GoPrimitiveDot /> Internship</h4>
       </div>
       <div>
         <p>HNG Internship 9 Front-End Task</p>
